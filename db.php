232545764f1858e3c5042f8483d5b910c93220f0
VickyBrowny <www.vchibuzor05@gmail.com>
2021-04-28 23:29:55 +0100

    cnnsite

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$mysql_host = 'localhost';
-$mysql_user = 'root';
-$mysql_pass = '';
-
-$mysql_db = 'data2';
-$link = mysqli_connect($mysql_host, $mysql_user, $mysql_pass, $mysql_db) or die('could not connect');
-?>
